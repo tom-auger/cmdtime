c4aa4b232adf86494046355e55b427f8ef8b9504
Tom Auger <tomauger@live.com>
2021-04-10 13:47:28 +0100

    Fix calculation of hours

--- cmdtime.plugin.zsh
+++ cmdtime.plugin.zsh
@@ -1,21 +1,20 @@
 zmodload zsh/datetime
 
 __cmdtime_current_time() {
-  zmodload zsh/datetime
   echo $EPOCHREALTIME
 }
 
 __cmdtime_format_duration() {
   local hours=$(printf '%.0f' $(($1 / 3600)))
-  local mins=$(printf '%.0f' $(($1 / 60)))
-  local secs=$(printf "%.3f" $(($1 - 60 * mins)))
+  local mins=$(printf '%.0f' $((($1 - hours * 3600) / 60)))
+  local secs=$(printf "%.3f" $(($1 - 60 * mins - 3600 * hours)))
 
   if [[ ! "${mins}" == "0" ]] || [[ ! "${hours}" == "0" ]]; then
       # If there are a non zero number of minutes or hours 
       # then display integer number of seconds
       secs=${secs%\.*}
   elif [[ "${secs}" =~ "^0\..*" ]]; then
-      # If secs starts with 0. i.e. is less than 0 then display
+      # If secs starts with 0. i.e. is less than 1 then display
       # the number of milliseconds instead. Strip off the leading 
       # zeros and append an 'm'. 
       secs="${${${secs#0.}#0}#0}m"
@@ -25,7 +24,6 @@ __cmdtime_format_duration() {
   fi
   local duration_str=$(echo "${hours}h:${mins}m:${secs}s")
   local format="${TIMER_FORMAT:-%d}"
-  #local format="%d"
   echo "${format//\%d/${${duration_str#0h:}#0m:}}"
 }
 
